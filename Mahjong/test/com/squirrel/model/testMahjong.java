27b60220f3316072daaf146a97102adfa9f8653a
active-admin <active-admin@LAPTOP-6HG7OFNT>
2016-08-05 17:10:38 +0200

    vgt

--- Mahjong/test/com/squirrel/model/testMahjong.java
+++ Mahjong/test/com/squirrel/model/testMahjong.java
@@ -9,12 +9,17 @@ import com.squirrel.app.Mahjong;
 public class testMahjong {
 
 	@Test
-	public void test() {
+	public void testDistributionMain() {
 		Mahjong mahjong=new Mahjong();
 		mahjong.initialiserUnePartie();
 		
 		assertEquals(14, mahjong.mainEst.handSize());
+		assertEquals(13, mahjong.mainOuest.handSize());
+		assertEquals(13, mahjong.mainNord.handSize());
+		assertEquals(13, mahjong.mainSud.handSize());
 	
 	}
+	
+	
 
 }
