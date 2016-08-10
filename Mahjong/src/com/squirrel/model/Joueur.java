ec68d78dc05192c358aa5f7dfe5f1e6b70922563
active-admin <active-admin@LAPTOP-6HG7OFNT>
2016-08-10 17:16:40 +0200

    joueur modif

--- Mahjong/src/com/squirrel/model/Joueur.java
+++ Mahjong/src/com/squirrel/model/Joueur.java
@@ -1,5 +1,7 @@
 package com.squirrel.model;
 
+import java.util.List;
+
 import com.squirrel.model.TuileVen.VenTuile;
 
 public class Joueur {
@@ -9,18 +11,32 @@ public class Joueur {
 	VenTuile vent;
 	boolean appel=false;
 	int penalite =0;
+	List<Tuile> bonus;
 	
 	public void incremPenalite() {
 		this.penalite ++;
 	}
 
-	public Joueur(String name, HandFacade hand,VenTuile vent){
+	public Joueur(String name, HandFacade hand,VenTuile vent, List<Tuile> bonus){
 		 super();
 		 this.name=name;
 		 this.hand=hand;
 		 this.vent=vent;
+		 this.bonus=bonus;
 	}
 	
+	public List<Tuile> getBonus() {
+		return bonus;
+	}
+
+	public void setHand(HandFacade hand) {
+		this.hand = hand;
+	}
+
+	public void setBonus(List<Tuile> bonus) {
+		this.bonus = bonus;
+	}
+
 	public void appel(){
 		appel=true;
 	}
