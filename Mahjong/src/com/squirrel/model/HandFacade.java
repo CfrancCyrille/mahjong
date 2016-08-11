711daed5a69b981e83ffbb1eb0359515737fd253
active_admin <active_admin@LAPTOP-ESST261B>
2016-08-11 15:09:24 +0200

    Modification de la facade de la main, vous avez accès a getCombinaisons!

--- Mahjong/src/com/squirrel/model/HandFacade.java
+++ Mahjong/src/com/squirrel/model/HandFacade.java
@@ -1,52 +1,74 @@
 package com.squirrel.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class HandFacade {
+
 	private Hand hand;
-	   //private ??? combi;
+	boolean mahjongPossible=false;
+	List<List<Tuile>> res;
 
-	   public HandFacade() {
-		   hand = new Hand();
-		 //??? combi;
-	   }
-	   
-
-	
-	
-public void getCombinaison(){
-	//fonction qui trie
-	//fonction qui groupe
-	//fonction qui voie les suites
-
-	//TODO : hand.combinaison(); //fonction qui fait correspondre les combi
-	//return combi; 
-}
-
-public Hand remplirMain(Tuile t){
-	try {
-		hand.fillHand(t);
-	} catch (MainPleineException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+/**
+ * Constructeur de la facade
+ */
+	public HandFacade() {
+		this.hand = new Hand();
+		this.res=new ArrayList<List<Tuile>>();
 	}
-	return  hand;
-}
 
-public Hand piocheDefausse(Tuile t){
-	boolean isCombi = false;
-	//isCombi=fnction de test qui renvoi isCombi
-	hand.pickDefausse(t,isCombi);
-	return hand;
-}
+	/**
+	 * Fonction permettant de trier les tuiles dans une liste de tuiles
+	 * Triées par familles (ordre alphabetique) puis par valeur
+	 */
+	public void triLesTuiles(List<Tuile> l){
+		hand.triTuiles(l);
+	}	
 
-public int handSize() {
-	return this.hand.tuilesListOfHand.size();
-}
-public Tuile get(int i){
-	return this.hand.tuilesListOfHand.get(i);
-}
-
-public void remove(Tuile t){
-	this.hand.tuilesListOfHand.remove(t);
-}
+	/**
+	 * Fonction permettant de trier les tuiles de la main, de former des groupes et de chercher des recombinaisons afin de maximiser l'efficacite de la recherche
+	 * Renvoie une liste des combinaisons trouvées (chacune est une liste)
+	 */
+	public List<List<Tuile>> getCombinaison(){
+		return res = hand.findCombinaisons(hand.tuilesListOfHand);
+	}
+	/**
+	 * Fonction permettant de remplir de tuiles un objet de type main
+	 */
+	public Hand remplirMain(Tuile t){
+		try {
+			hand.fillHand(t);
+		} catch (MainPleineException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return  hand;
+	}
+	/**
+	 * Fonction permettant de rajouter une tuile défaussée à la main, si elle forme une combinaison
+	 */
+	public Hand piocheDefausse(Tuile t){
+		boolean isCombi = false;
+		//TODOisCombi=fnction de test qui renvoi isCombi
+		hand.pickDefausse(t,isCombi);
+		return hand;
+	}
+	/**
+	 * Fonction permettant de récupérer la taille de la main
+	 */
+	public int handSize() {
+		return this.hand.tuilesListOfHand.size();
+	}
+	/**
+	 * Fonction permettant de récupérer la ième tuile de la main
+	 */
+	public Tuile get(int i){
+		return this.hand.tuilesListOfHand.get(i);
+	}
+	/**
+	 * Fonction permettant de retirer une tuile de la main
+	 */
+	public void remove(Tuile t){
+		this.hand.tuilesListOfHand.remove(t);
+	}
 }
