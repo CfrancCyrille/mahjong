352ec6801e81914c5f61d48c2045aa1299f18dbb
active-admin <active-admin@LAPTOP-6HG7OFNT>
2016-08-11 14:02:46 +0200

    Creation classe testMahjongPartie

---
+++ Mahjong/test/com/squirrel/app/TestMahjongPartie.java
@@ -0,0 +1,14 @@
+package com.squirrel.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMahjongPartie {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
