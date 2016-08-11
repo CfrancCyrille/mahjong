3e106bdbc29b235f9628eb6c4ca066b5dc8d11aa
active-admin <active-admin@LAPTOP-6HG7OFNT>
2016-08-11 14:17:45 +0200

    test jetteTuile OK

--- Mahjong/test/com/squirrel/app/TestMahjongPartie.java
+++ Mahjong/test/com/squirrel/app/TestMahjongPartie.java
@@ -5,10 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestMahjongPartie {
+	
+	MahjongInitialisation mah;
+	MahjongPartie mahp;
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		mah=new MahjongInitialisation();
+		mah.initialiserUnePartie();
+		mahp = new MahjongPartie();
+		
+		mahp.jetteTuile(mah.jEst, mah.jEst.getHand().get(1));
+		assertEquals(13, mah.jEst.getHand().handSize());
+		assertEquals(1, mahp.defausse.size());
 	}
 
 }
